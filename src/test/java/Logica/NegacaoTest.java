cefad1ace2fc6e3de234fb8931a4ec33212160c5
Felipe Keiler <36575953+fkeiler@users.noreply.github.com>
2022-02-04 14:00:45 -0300

    Testado método toString da Negação

--- src/test/java/Logica/NegacaoTest.java
+++ src/test/java/Logica/NegacaoTest.java
@@ -4,6 +4,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+class alwaysA implements Proposicao {
+    @Override
+    public boolean valorar() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "A";
+    }
+}
+
 class NegacaoTest {
 
     @Test
@@ -16,5 +28,12 @@ class NegacaoTest {
     void valorarWorksOnFalseStatements() {
         Negacao n = new Negacao(new alwaysFalse());
         assertTrue(n.valorar());
+    }
+
+    @Test
+    void toStringWorks() {
+        Negacao n = new Negacao(new alwaysA());
+        String expected = "¬A";
+        assertEquals(expected, n.toString());
     }
 }
